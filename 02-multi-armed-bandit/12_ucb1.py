cc943c555774ab330a392407689437bcbf512e30
Glenn Thomas <glenn.thomas42@gmail.com>
2020-02-09 14:49:28 +1300

    Added ucb1, updated number of tries to 100k

--- 02-multi-armed-bandit/12_ucb1.py
+++ 02-multi-armed-bandit/12_ucb1.py
@@ -3,14 +3,14 @@ import numpy as np
 import pdb
 import matplotlib.pyplot as plt
 
-# Implements optimisitic initial values
+# Implements UCB1 - sets an upper bound based on number of trials
 
-def run_experiment(mu, upper_limit, N):
+def run_experiment(mu, N):
     # Make bandits
     n_bandits = len(mu)
     bandits = list()
     for i in range(n_bandits):
-        bandits.append(bandit.Bandit(mu[i], upper_limit))
+        bandits.append(bandit.Bandit(mu[i],0))
     
     # Reward vector
     rewards = np.zeros(N)
@@ -18,8 +18,11 @@ def run_experiment(mu, upper_limit, N):
     # Run simulation
     for i in range(N):
         means = np.array([b.mean for b in bandits])
-        j = np.argmax(means)
+        # Add a small number to deal with the zero case
+        attempts = np.array([b.N for b in bandits]) + 0.1
+        j = np.argmax(means + np.sqrt(2*np.log(i + 1)/attempts))
         rewards[i] = bandits[j].pull()
+        # TODO Using an initial value of zero - how does this bias the method?
     
        # print(j)
        # print([b.mean for b in bandits])
@@ -33,13 +36,13 @@ def run_experiment(mu, upper_limit, N):
 
 # Plot it
 for b in range(5):
-    data = run_experiment(mu=[-1.0, 0.0, 1.0], upper_limit=10, N=100000)
+    data = run_experiment(mu=[-1.0, 0.0, 1.0], N=100000)
     if(b == 0):
-        plt.plot(data, label='UCB1', alpha = 0.3, color = "red")
+        plt.plot(data, label='Optimistic Initial Values', alpha = 0.3, color = "red")
     else:
         plt.plot(data, alpha = 0.3, color = "red")
 plt.legend()
 plt.xscale('log')
-plt.savefig('output/11-optimistic_initial_values.png')
+plt.savefig('output/12-ucb1.png')
 
 print(data[-1])
