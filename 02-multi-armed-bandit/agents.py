1e9761922472ecc8858e76ef9cfb7c2e223f7099
Glenn Thomas <glenn@harmonic.co.nz>
2020-02-12 23:58:05 +1300

    Refactored code

---
+++ 02-multi-armed-bandit/agents.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+class epsilon_greedy:
+
+    def __init__(self, n_bandits, eps):
+        self.n_bandits = n_bandits
+        self.means = np.repeat(0.0, n_bandits)
+        self.bandit_N = np.repeat(0, n_bandits)
+        self.eps = eps
+        self.last_action = None
+        self.first_run = True
+
+    def choose_bandit(self):
+        # This version explores the first turn and choose the max of only those it has explored.
+
+        p = np.random.random()
+        if self.first_run:
+            j = np.random.choice(self.n_bandits)
+            self.first_run = False
+        elif p < self.eps:
+            j = np.random.choice(self.n_bandits)
+        else:
+            ind_tried_bandits = self.bandit_N > 0
+            # Take the max of bandits who we tried
+            j = np.argmax(self.means[ind_tried_bandits])
+            j = np.where(ind_tried_bandits)[0][j]
+            j = np.argmax(self.means)
+        
+        self.last_action = j
+        return j
+
+    def update(self, reward):
+        j = self.last_action
+        self.bandit_N[j] = self.bandit_N[j] + 1
+        self.means[j] = (1-1.0/self.bandit_N[j])*self.means[j] + 1.0/self.bandit_N[j]*reward
+
+class adaptive_epsilon_greedy:
+    # Uses decaying epsilon = 1/n
+
+    def __init__(self, n_bandits):
+        self.n_bandits = n_bandits
+        self.means = np.repeat(0.0, n_bandits)
+        self.bandit_N = np.repeat(0, n_bandits)
+        self.last_action = None
+        self.first_run = True
+
+    def choose_bandit(self):
+        # This version explores the first turn and choose the max of only those it has explored.
+
+        p = np.random.random()
+        if self.first_run:
+            j = np.random.choice(self.n_bandits)
+            self.first_run = False
+        elif p < 1.0/(np.sum(self.bandit_N)+1):
+            j = np.random.choice(self.n_bandits)
+        else:
+            ind_tried_bandits = self.bandit_N > 0
+            # Take the max of bandits who we tried
+            j = np.argmax(self.means[ind_tried_bandits])
+            j = np.where(ind_tried_bandits)[0][j]
+            j = np.argmax(self.means)
+        
+        self.last_action = j
+        return j
+
+    def update(self, reward):
+        j = self.last_action
+        self.bandit_N[j] = self.bandit_N[j] + 1
+        self.means[j] = (1-1.0/self.bandit_N[j])*self.means[j] + 1.0/self.bandit_N[j]*reward
+
+class optimistic_initial_values:
+
+    def __init__(self, n_bandits, initial_value):
+        self.n_bandits = n_bandits
+        self.means = np.repeat(0.0, n_bandits)
+        self.bandit_N = np.repeat(1, n_bandits)
+        self.last_action = None
+        self.means[:] = initial_value
+
+    def choose_bandit(self):
+        j = np.argmax(self.means)
+        self.last_action = j
+        return j
+
+    def update(self, reward):
+        j = self.last_action
+        self.bandit_N[j] = self.bandit_N[j] + 1
+        self.means[j] = (1-1.0/self.bandit_N[j])*self.means[j] + 1.0/self.bandit_N[j]*reward
+
+class ucb1:
+
+    def __init__(self, n_bandits):
+        self.n_bandits = n_bandits
+        self.means = np.repeat(0.0, n_bandits)
+        self.bandit_N = np.repeat(0, n_bandits)
+        self.last_action = None
+
+    def choose_bandit(self):
+        j = np.argmax(self.means + np.sqrt(2*np.log(np.sum(self.bandit_N) + 1)/(self.bandit_N+0.1)))
+        self.last_action = j
+        return j
+
+    def update(self, reward):
+        j = self.last_action
+        self.bandit_N[j] = self.bandit_N[j] + 1
+        self.means[j] = (1-1.0/self.bandit_N[j])*self.means[j] + 1.0/self.bandit_N[j]*reward
---
+++ 02-multi-armed-bandit/main.py
@@ -0,0 +1,77 @@
+# Glenn Thomas
+# 2020-02-12
+# Function to run different strategies for the bandit problem
+
+import bandit
+import agents
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Bandit means
+mu = np.array([-1, 0, 1])
+N = 100000
+n_bandits = len(mu)
+
+def run_experiment(mu, N, agent):
+    """
+    Runs the expirement
+
+    Inputs:
+    mu - numpy array of means for bandaits
+    N - number of turns
+    agent - a class of agent with methods choose_bandit and update.
+
+    Output
+    """
+    # Make bandits
+    n_bandits = len(mu)
+    bandits = list()
+    for i in range(n_bandits):
+        bandits.append(bandit.Bandit(mu[i]))
+   
+    # Reward vector (could leave this to the agent)
+    rewards = np.zeros(N)
+
+    # Run simulation
+    for i in range(N):
+        j = agent.choose_bandit()
+        reward = bandits[j].pull()
+        agent.update(reward)
+        rewards[i] = reward
+
+    # Calculate average
+    cumulative_average = np.cumsum(rewards)/(np.arange(N) + 1)
+    return(cumulative_average)
+
+### Epsilon greedy siumulation
+
+for b in range(5):
+    eps01 = run_experiment(mu, 10000, agents.epsilon_greedy(n_bandits, 0.01))
+    eps10 = run_experiment(mu, 10000, agents.epsilon_greedy(n_bandits, 0.1))
+    eps30 = run_experiment(mu, 10000, agents.epsilon_greedy(n_bandits, 0.3))
+    if(b == 0):
+        plt.plot(eps01, label='eps = 0.01', alpha = 0.3, color = "red")
+        plt.plot(eps10, label='eps = 0.1', alpha = 0.3, color = "green")
+        plt.plot(eps30, label='eps = 0.3', alpha = 0.3, color = "blue")
+    else:
+        plt.plot(eps01, alpha = 0.3, color = "red")
+        plt.plot(eps10, alpha = 0.3, color = "green")
+        plt.plot(eps30, alpha = 0.3, color = "blue")
+
+plt.legend()
+plt.xscale('log')
+plt.savefig('output/comparing-epsilons.png')
+
+eps10 = run_experiment(mu, N, agents.epsilon_greedy(n_bandits, 0.1))
+eps_adaptive = run_experiment(mu, N, agents.adaptive_epsilon_greedy(n_bandits))
+optimistic_initial_values = run_experiment(mu, N, agents.optimistic_initial_values(n_bandits, 10.0))
+ucb1 = run_experiment(mu, N, agents.ucb1(n_bandits))
+
+plt.figure()
+plt.plot(eps10, label = "Epsilon Greedy 0.1")
+plt.plot(eps_adaptive, label = "Decaying Epsilon Greedy")
+plt.plot(optimistic_initial_values, label = "Optimistic Initial Values")
+plt.plot(ucb1, label = "UCB1")
+plt.legend()
+plt.xscale('log')
+plt.savefig('output/comparing_all_methods.png')
