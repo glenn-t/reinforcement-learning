4e98502ade34e0545fafcf39b73ee8afaaf85795
Glenn Thomas <glenn.thomas42@gmail.com>
2020-02-23 16:43:50 +1300

    trying to get it working in docker. Some issue with interactive session

--- 04-tic-tac-toe/main.R
+++ 04-tic-tac-toe/main.R
@@ -5,13 +5,19 @@ lapply(list.files("R", full.names = TRUE), source)
 # Generate all states
 all_states = get_all_states_and_winner()
 
-# Human vs human
+# # Human vs human
+# human = new_human()
+# draw_board(1:9, instructions = TRUE)
+# play_game(list(human, human), draw = TRUE, all_states)
+# 
+# # Random vs Random
+# agent_random = new_agent_random()
+# draw_board(1:9, instructions = TRUE)
+# play_game(list(agent_random, agent_random), draw = TRUE, all_states)
+
+# Human vs agent
+agent_random = new_agent_random()
 human = new_human()
 draw_board(1:9, instructions = TRUE)
-play_game(list(human, human), draw = TRUE, all_states)
-
-# Random vs Random
-agent_random = new_agent_random()
-draw_board(1:9, instructions = TRUE)
-play_game(list(agent_random, agent_random), draw = TRUE, all_states)
+play_game(list(agent_random, human), draw = TRUE, all_states)
 
