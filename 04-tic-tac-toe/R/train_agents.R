8966bbaaeb45e6a6c8f31ba30a09a4632972cfdc
Glenn Thomas <glenn@harmonic.co.nz>
2020-02-27 11:56:02 +1300

    Print info on score not prop won

--- 04-tic-tac-toe/R/train_agents.R
+++ 04-tic-tac-toe/R/train_agents.R
@@ -5,9 +5,10 @@ train_agents = function(N, p1, p2, draw = FALSE) {
     res = play_game(list(p1, p2), draw = draw, all_states)
     p1 = update_agent(p1, res$state_history)
     p2 = update_agent(p2, res$state_history)
+    res$winner[res$winner == 2] = -1
     winner[i] = res$winner
     if(i %% 200 == 0) {
-      cat(i, mean(winner[1:i] == 1), mean(winner[(i-199):i] == 1), "\n")
+      cat(i, mean(winner[1:i]), mean(winner[(i-199):i]), "\n")
     }
   }
   return(list("p1" = p1, "p2" = p2, winner = winner))
