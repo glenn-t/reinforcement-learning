9ace1380a5b1764acc810b76a800930c31339f99
Glenn Thomas <glenn.thomas42@gmail.com>
2020-02-28 02:25:09 +1300

    Show draw, cross, circle wins instead of ratio

--- 04-tic-tac-toe/R/train_agents.R
+++ 04-tic-tac-toe/R/train_agents.R
@@ -8,7 +8,8 @@ train_agents = function(N, p1, p2, draw = FALSE) {
     res$winner[res$winner == 2] = -1
     winner[i] = res$winner
     if(i %% 200 == 0) {
-      cat(i, mean(winner[1:i]), mean(winner[(i-199):i]), "\n")
+      ind = (i-199):i
+      cat(i, sum(winner[ind] == 0), sum(winner[ind] == 1), sum(winner[ind] == -1),mean(winner[1:i]),  "\n")
     }
   }
   return(list("p1" = p1, "p2" = p2, winner = winner))
