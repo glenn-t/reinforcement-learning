a2b36e825edb83e040911bf3cbfd1b5c9489c6cb
Glenn Thomas <glenn@harmonic.co.nz>
2020-04-10 09:44:49 +1200

    Completed grid_world.py

---
+++ 06-dynamic-programming/grid_world.py
@@ -0,0 +1,119 @@
+import numpy as np
+
+class Grid:
+    def __init__(self, start, blocks, rewards, terminal_states):
+        """
+        start: tuple (i, j) start position
+        blocks: numpy logical array of blocks
+        rewards: numpy float array of rewards
+        terminal_states: numpy logical array of terminal states
+        """
+        self.height = terminal_states.shape[0] # i
+        self.width = terminal_states.shape[1] # j
+        self.i = start[0]
+        self.j = start[1]
+        self.previous_i = np.nan
+        self.previous_j = np.nan
+
+        self.rewards = rewards
+        self.terminal_states = terminal_states
+        self.blocks = blocks
+
+        # Create actions
+        self.actions = {}
+        for i in range(self.height):
+            for j in range(self.width):
+                if(not blocks[i,j]):
+                    key = (i, j)
+                    a = np.array(["D", "U", "L", "R"])
+                    # Check for walls
+                    if(j == 0):
+                        a = a[a != "L"]
+                    if(j == (self.width - 1)):
+                        a = a[a != "R"]
+                    if(i == 0):
+                        a = a[a != "U"]
+                    if(i == (self.height - 1)):
+                        a = a[a != "D"]
+                    # Now check for blocks in possible action spaces
+                    if ("D" in a) and blocks[i + 1, j]:
+                        a = a[a != "D"]
+                    if ("U" in a) and blocks[i - 1, j]:
+                        a = a[a != "U"]
+                    if ("L" in a) and blocks[i, j - 1]:
+                        a = a[a != "L"]
+                    if ("R" in a) and blocks[i, j + 1]:
+                        a = a[a != "R"]
+                    self.actions[key] = a
+
+    def set_state(self, state):
+        # state is a tuple of i,j coords (e.g. (0, 1))
+        self.i = state[0]
+        self.j = state[1]
+
+    def current_state(self):
+        return((self.i, self.j))
+
+    def is_terminal(self):
+        return(self.terminal_states[self.i, self.j])
+
+    def move(self, action):
+        self.previous_i = self.i
+        self.previous_j = self.j
+        """
+        action: either "U", "D", "L", "R"
+        """
+        if (action in self.actions[(self.i, self.j)]):
+            # Do action
+            if action == "D":
+                self.i = self.i + 1
+            elif action == "L":
+                self.j = self.j - 1
+            elif action == "R":
+                self.j = self.j + 1
+            elif action == "U":
+                self.i = self.i - 1
+        else:
+            raise ValueError("Invalid action")
+
+    def undo_move(self):
+        """Undos the most recent move"""
+        self.i = self.previous_j
+        self.j = self.previous_j
+
+    def game_over(self):
+        return(self.is_terminal())
+
+    def all_states(self):
+        """returns a list of all states except blocks"""
+        return(list(self.actions.keys()))
+
+def standard_grid():
+    "Returns standard grid game"
+    width = 4
+    height = 3
+    blocks = np.zeros(shape = (height, width), dtype = bool)
+    blocks[1,1] = True
+
+    rewards = np.zeros(shape = (height, width))
+    rewards[0, 3] = 1
+    rewards[1, 3] = -1
+
+    terminal_states = np.zeros(shape = (height, width), dtype = bool)
+    terminal_states[0:2, 3] = True
+
+    return Grid(
+        start=(0, 0),
+        blocks=blocks,
+        rewards=rewards,
+        terminal_states=terminal_states,
+    )
+
+def negative_grid(step_reward=-0.1):
+    "Returns grid game with negative step rewards"
+    g = standard_grid()
+    g.rewards = g.rewards + step_reward
+    return(g)
+
+print(standard_grid().rewards)
+print(negative_grid().rewards)
---
+++ 06-dynamic-programming/main.py
@@ -0,0 +1,5 @@
+# Glenn Thomas
+# 2020-04-09
+# Function to run different strategies for the bandit problem
+
+print("hi")
