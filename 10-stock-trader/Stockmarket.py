24f8c83908a64be0f28a6c197a7c832ea8ce0663
Glenn Thomas <glenn.thomas42@gmail.com>
2020-07-07 00:24:40 +1200

    Completed Stockmarket class

--- 10-stock-trader/Stockmarket.py
+++ 10-stock-trader/Stockmarket.py
@@ -7,12 +7,16 @@ Created on Mon Jul  6 21:56:27 2020
 """
 
 import pandas as pd
+import numpy as np
+
 
 class StockMarket:
     
-    def __init__(self, mode):
-        self.mode = mode
+    def __init__(self, mode, initial_investment):
         assert mode in ["test", "train"]
+        self.mode = mode
+        self.initial_investment = initial_investment
+        
         
         # Load data
         self.data = pd.read_csv("aapl_msi_sbux.csv")
@@ -22,4 +26,75 @@ class StockMarket:
         if mode == "train":
             self.data = self.data.loc[0:split_point, ]
         else:
-            self.data = self.data.loc[(split_point:, ]
+            self.data = self.data.loc[split_point:, ]
+            
+        # Convert data to matrix
+        self.data = np.array(self.data)
+        
+        # Setup portfolio
+        self.holdings = np.zeros(self.data.shape[1])
+        self.state = np.zeros(self.data.shape[1]*2 + 1)
+        
+        # Setup initial variables
+        self.reset()
+        self.max_t = len(self.data) - 1
+        
+    def set_state(self):
+        self.state[0:3] = self.prices
+        self.state[3:6] = self.holdings
+        self.state[6] = self.cash
+        
+    def reset(self):
+        self.t = 0
+        self.prices = self.data[self.t,:]
+        self.holdings[:] = 0
+        self.cash = self.initial_investment
+        self.value = self.initial_investment
+        self.done = False
+        self.set_state()
+        return(self.state)
+        
+
+    def step(self, action):
+        assert not self.done
+        assert type(action) is list
+        assert len(action) == 3
+        for a in action:
+            assert a in ["BUY", "HOLD", "SELL"]
+        
+        ## First sell shares at current price
+        for i, a in enumerate(action):
+            if a == "SELL":
+                self.cash = self.cash + self.holdings[i]*self.prices[i]
+                self.holdings[i] = 0
+                
+        ## Second buy shares in round robin fashin
+        if "BUY" in action:
+           # Create indicator variable to check if bought this round
+           bought = True
+           while bought:
+               bought = False
+               for i, a in enumerate(action):
+                   if (a == "BUY") and ( self.prices[i] <= self.cash):
+                       self.holdings[i] += 1
+                       self.cash -= self.prices[i]
+                       bought = True
+                       
+        ## Increment prices
+        self.t = self.t + 1
+        self.done = self.t == self.max_t
+        self.prices = self.data[self.t,:]
+        # Calculate new portfolio value
+        new_value = np.sum(self.holdings*self.prices) + self.cash
+        reward = new_value - self.value
+        self.value = new_value
+        self.set_state()
+                       
+        ## Return information
+        return((self.state, reward, self.done, {"portfolio_value":self.value}))
+
+       
+       
+    
+                                       
+                                       
--- 10-stock-trader/main.py
+++ 10-stock-trader/main.py
@@ -7,6 +7,17 @@ Created on Mon Jul  6 21:59:54 2020
 """
 
 import Stockmarket
+import numpy as np
 
-train_env = Stockmarket.StockMarket("train")
+INITIAL_INVESTMENT = 20000.0
 
+train_env = Stockmarket.StockMarket("test", INITIAL_INVESTMENT)
+done = False
+state = train_env.reset()
+while not done:
+    # Random agent
+    action = np.random.choice(["BUY", "SELL", "HOLD"], size = 3).tolist()
+    next_state, reward, done, info = train_env.step(action)
+    state = next_state
+
+print(info)
